2d1540af73b96f04269c4edb422e6190913d0681
Sanjaikumaran <sanjaikumaran0311@gmail.com>
2024-08-13 01:51:38 +0530

    Updated

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
@@ -28,8 +30,50 @@ function getLocalIPs() {
   return localIPs;
 }
 
+// Function to get all folders and their images in a directory as a tree array
+function getFolderTree(directory) {
+  function walkDir(currentPath) {
+    const items = fs.readdirSync(currentPath);
+    const folder = {
+      name: path.basename(currentPath),
+      images: [],
+      children: [],
+    };
+
+    items.forEach((item) => {
+      const itemPath = path.join(currentPath, item);
+      const stats = fs.statSync(itemPath);
+
+      if (stats.isDirectory()) {
+        const childFolder = walkDir(itemPath);
+        if (childFolder.images.length > 0 || childFolder.children.length > 0) {
+          folder.children.push(childFolder);
+        }
+      } else if (stats.isFile() && isImageFile(item)) {
+        folder.images.push(item);
+      }
+    });
+
+    return folder;
+  }
+
+  function isImageFile(fileName) {
+    const ext = path.extname(fileName).toLowerCase();
+    return [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp"].includes(ext);
+  }
+
+  const rootFolder = walkDir(directory);
+  return rootFolder.children.filter((folder) => folder.images.length > 0);
+}
+
 const localIPs = getLocalIPs();
 const port = 8000;
+
+// Endpoint to get the folder tree
+app.get("/folder-tree", (req, res) => {
+  const folderTree = getFolderTree(path.join(__dirname, "assets"));
+  res.json(folderTree);
+});
 
 app.get("/config", (req, res) => {
   res.json({ localIPs, port });
@@ -59,7 +103,7 @@ server.listen(port, () => {
   const platform = process.platform;
   const lastIP = localIPs[localIPs.length - 1].address;
   if (lastIP) {
-    const url = `http://${lastIP}:${port}/video.html`;
+    const url = `http://${lastIP}:${port}/slideshow.html`;
     if (platform === "win32") {
       exec(`powershell -NoProfile -Command "Start-Process '${url}'"`); // For Windows
     } else if (platform === "darwin") {
